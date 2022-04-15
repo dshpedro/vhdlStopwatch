2517d4bdb945d741859f8b6a0bc56b4be7fb25b1
predo4ter <pedrohpisantos@gmail.com>
2022-04-15 17:06:56 -0300

    latchRS working in main

--- main.vhd
+++ main.vhd
@@ -1,26 +1,38 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.std_logic_arith.all;
-use ieee.std_logic_unsigned.all; -- operador """+"""
--- possibilita o incremento da saída
 
 entity main is
     port(
-        CLK, R : in std_logic;
-        Q : buffer std_logic_vector(0 to 3)
+        Button0, Button1, Button2, Clock_50 : in std_logic;
+        HEX3_D, HEX2_D, HEX1_D, HEX0_D : out std_logic_vector(0 to 6)
     );
 end main;
 
 architecture behav of main is
+
+component latchRS is
+    port
+    (
+        S, R : in std_logic; -- S = PIN_F1; R = PIN_G3;
+        Q : out std_logic
+    );
+end component;
+
+component counter is
+    port(
+        CLK, R : in std_logic;
+        Q : buffer std_logic_vector(0 to 3)
+    );
+end component;
+
+signal latQ, counterReset : std_logic;
+signal phvector : std_logic_vector(0 to 3);
 begin
-    process(CLK, R)
-    begin
+
+    lat : latchRS port map ( S => Button2, R => Button1, Q => latQ );
+    counter3 : counter port map ( CLK => counterReset, R => counterReset  , Q => phvector);
+
+    counterReset <= (not Button0) or (not latQ); 
     
-    if R = '1' then
-        Q <= "0000";
-    elsif rising_edge(CLK) then
-        Q <= Q + 1;
-    end if;
     
-    end process;
 end behav;
