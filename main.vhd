27c6459921c01fe7adf1c5f86ae0c39f16cf8efc
predo4ter <pedrohpisantos@gmail.com>
2022-04-15 16:20:00 -0300

    restructure (again)

--- main.vhd
+++ main.vhd
@@ -1,27 +1,26 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all; -- operador """+"""
+-- possibilita o incremento da saída
 
 entity main is
     port(
-        dcba : in std_logic_vector(0 to 3);
-        mainOut : out std_logic_vector(0 to 6)
+        CLK, R : in std_logic;
+        Q : buffer std_logic_vector(0 to 3)
     );
 end main;
 
 architecture behav of main is
 begin
-
-    with dcba select
-    mainOut <= "0000001" when "0000",
-                  "1001111" when "0001",
-                  "0010010" when "0010",
-                  "0000110" when "0011",
-                  "1001100" when "0100",
-                  "0100100" when "0101",
-                  "0100000" when "0110",
-                  "0001111" when "0111",
-                  "0000000" when "1000",
-                  "0000110" when "1001",
-                  "1111111" when others;
-
+    process(CLK, R)
+    begin
+    
+    if R = '1' then
+        Q <= "0000";
+    elsif rising_edge(CLK) then
+        Q <= Q + 1;
+    end if;
+    
+    end process;
 end behav;
