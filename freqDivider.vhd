3721739214bf6773b6a5645e2c7360256c309e98
predo4ter <pedrohpisantos@gmail.com>
2022-04-16 10:43:03 -0300

    counters with wrong clock

--- freqDivider.vhd
+++ freqDivider.vhd
@@ -3,7 +3,7 @@ use ieee.std_logic_1164.all;
 
 entity freqDivider is
     port(
-    clk50M : in std_logic; -- clk50M = PIN_G21
+    clkIN : in std_logic; -- clkIN = PIN_G21
     freqOut : buffer std_logic
     );
 end freqDivider;
@@ -12,10 +12,10 @@ architecture behav of freqDivider is
 signal i : integer := 1;
 begin
 
-    process(clk50M, freqOut)
+    process(clkIN, freqOut)
     begin
         
-    if rising_edge(clk50M) then
+    if rising_edge(clkIN) then
         i <= i + 1;
     
     if i = 50000000 then
--- main.vhd
+++ main.vhd
@@ -10,7 +10,7 @@ end main;
 
 architecture behav of main is
 
-component latchRS is
+component latchRS is -- Latch RS
     port
     (
         S, R : in std_logic; -- S = PIN_F1; R = PIN_G3;
@@ -18,21 +18,41 @@ component latchRS is
     );
 end component;
 
-component counter is
+component freqDivider is -- divisor de frequência 1/50 000 000
+    port(
+    clkIN : in std_logic; -- clkIN = PIN_G21
+    freqOut : buffer std_logic
+    );
+end component;
+
+component counter is  -- contador bcd de 4 bits
     port(
         CLK, R : in std_logic;
         Q : buffer std_logic_vector(0 to 3)
     );
 end component;
 
-signal latQ, counterReset : std_logic;
-signal phvector : std_logic_vector(0 to 3);
+signal latQ : std_logic; -- saída Q do latch
+signal counterReset : std_logic; -- reset do contador
+
+signal oneHz : std_logic; -- f = 1Hz
+signal clkCounter0 : std_logic; -- clock do contador menos significativo
+
+signal phvector : std_logic_vector(0 to 3); -- 4 bits placeholder vector
 begin
 
-    lat : latchRS port map ( S => Button2, R => Button1, Q => latQ );
-    counter3 : counter port map ( CLK => counterReset, R => counterReset  , Q => phvector);
 
+    lat : latchRS port map ( S => Button2, R => Button1, Q => latQ );
+    
+    freqDiv : freqDivider port map ( clkIN => Clock_50 , freqOut => oneHz );
+    
+    counter3 : counter port map ( CLK => clkCounter0, R => counterReset  , Q => phvector);
+    counter2 : counter port map ( CLK => clkCounter0, R => counterReset  , Q => phvector);
+    counter1 : counter port map ( CLK => clkCounter0, R => counterReset  , Q => phvector);
+    counter0 : counter port map ( CLK => clkCounter0, R => counterReset  , Q => phvector);
+    
     counterReset <= (not Button0) or (not latQ); 
     
+    clkCounter0 <= latQ and oneHz;
     
 end behav;
